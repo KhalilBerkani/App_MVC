04b15580f709830afe21edeb14a4b4917123e8a5
Khalil Mohammed Berkani <khalilikafo@gmail.com>
2024-02-12 18:20:01 +0100

    Creation de Categories.cs

---
+++ AppInd/Models/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppInd.Models
+{
+    public class Categories
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
