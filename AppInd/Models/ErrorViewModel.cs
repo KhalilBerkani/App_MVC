fd2c58a3753008123d609966f15b18d8bc9c9ae0
Khalil Mohammed Berkani <khalilikafo@gmail.com>
2024-02-12 16:49:11 +0100

    Ajoutez des fichiers projet.

---
+++ AppInd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppInd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
