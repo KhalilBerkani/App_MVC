31900bcfbd0ef8c5a8b077bbc14cdab7c5cb1aa4
Khalil Mohammed Berkani <khalilikafo@gmail.com>
2024-02-12 19:13:55 +0100

    configuration complet du db

--- AppInd/Data/ApplicationDbContext.cs
+++ AppInd/Data/ApplicationDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace AppInd.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppInd.Data
 {
-    public class ApplicationDbContext 
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
     }
 }
