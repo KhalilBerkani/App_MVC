a47be73824128c93970658550eda73bf82c8cbaf
Khalil Mohammed Berkani <khalilikafo@gmail.com>
2024-02-20 11:14:24 +0100

    finition de edit + ajoute de delete

--- AppInd/Models/Categories.cs
+++ AppInd/Models/Categories.cs
@@ -8,9 +8,11 @@ namespace AppInd.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(20)]
         [DisplayName("Nom du Categorie")]
         public string Name { get; set; }
         [DisplayName("Ordre du Categorie")]
+        [Range(1,20,ErrorMessage ="Entrer un nombre entre 1 et 20 ")]
         public int DisplayOrder { get; set; }
     }
 }
